ae1d08d9d6009bcb8053f1139935fb65979296d5
Replit user <>
2023-08-16 14:15:17 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -90,7 +90,7 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int result[]
         }
   }
     
-  for (; i < size1; i++, k++) {
+  for (i=0; i < size1; i++, k++) {
       result[k] = arr1[i];
   }
     
