674fa0c0281a43c313e01c97e358febe1765987a
Replit user <>
2023-08-16 14:06:11 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -80,7 +80,7 @@ arreglos en un tercer arreglo también ordenado.
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int result[]){
   int i, j, k;
     
-  for (i = 0, j = 0, k = 0; i < tamaño1 && j < tamaño2; k++) {
+  for (i = 0, j = 0, k = 0; i < size && j < size2; k++) {
       if (arr1[i] < arr2[j]) {
           result[k] = arr1[i];
           i++;
