50017e56997caa0bc9ae2817ee295a994a37d904
Replit user <>
2023-08-17 20:16:30 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -79,9 +79,9 @@ ordenados y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado.
 */
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,int result[]){
-  //int i,j,k;
+  int i,j,k;
     
-  for (int i = 0,int j = 0,int k = 0; i < size1 && j < size2; k++) {
+  for (i = 0,j = 0,k = 0; i < size1 && j < size2; k++) {
       if (arr1[i] < arr2[j]) {
           result[k] = arr1[i];
           i++;
